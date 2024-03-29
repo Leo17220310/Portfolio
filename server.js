ba003e7f208f318fe007f0310bc88f96889484c3
Leo17220310 <125191929+Leo17220310@users.noreply.github.com>
2024-03-29 15:30:45 +0100

    new

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000; // Wählen Sie einen freien Port
 
 // Statische Dateien wie HTML, CSS und JavaScript aus dem öffentlichen Ordner bereitstellen
-app.use(express.static('public'));
+app.use(express.static('docs'));
 
 // Starten Sie den Server und geben Sie eine Bestätigungsmeldung aus
 app.listen(port, () => {
